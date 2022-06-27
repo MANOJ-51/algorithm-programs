1b328f4f7a4f441fe6f8d244a7b7d4c628071d54
MANOJ-51 <m4anoj51@gmail.com>
2022-06-27 22:58:16 +0530

    added: method to sort using insertion sort

---
+++ com/bridgelabz/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class InsertionSort {
+    //insertion sorting method
+    public static String[] sorting(String array[], int length) {
+        String temp;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    //main method
+    public static void main(String[] args) {
+        String[] array = {"manoj", "sai", "ram", "ramesh"};
+        String sortedArray[] = sorting(array, array.length);
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+    }
+}
